1ecb81b5a035aad091ac657fdbfdce50c008ecf6
OlegBogovik <olegbogovik12.qatester@gmail.com>
2023-12-06 21:41:02 +0200

    new page object elements

--- tests/registration.spec.js
+++ tests/registration.spec.js
@@ -2,13 +2,13 @@ import { test, expect } from '@playwright/test';
 import { Login } from './pageObjects/login-page';
 import { Registration } from './pageObjects/registration-page';
 
-const email = 'borisbritva6996+1@gmail.com';
+const email = 'borisbritva6996@gmail.com';
 const password = '12345!Hur';
 const phoneNumber = '+380979531272';
+const factoryName = 'New Factory Name';
+const numbersVallues = '263'
 
-
-
-test.only('Register a new valid user', async ({ page }) => {
+test.only('Register a user with existing email', async ({ page }) => {
     const loginPage = new Login(page);
     const registrPage = new Registration(page);
     const address = 'Ukaraine, Ternopil 11';
@@ -27,7 +27,7 @@ test.only('Register a new valid user', async ({ page }) => {
     expect (await page).toHaveURL('https://dar-ui-supplier.dev-test.pro/auth/register');
 
     //Factories details page
-    await registrPage.factoryNameFld.fill(email);
+    await registrPage.factoryNameFld.fill(factoryName);
     await registrPage.cityFld.fill(email);
 
     //Drop down
@@ -36,15 +36,21 @@ test.only('Register a new valid user', async ({ page }) => {
     
     await registrPage.latitudeFld.fill(email);
     await registrPage.longitudeFld.fill(email);
+    await registrPage.annualEnergElectricFld.fill(numbersVallues);
     //Drop down
     await registrPage.insertelEctrisityDropD.click(); 
     await registrPage.europe.click(); // paramiter of dropdown
-    
-    await registrPage.annualEnergElectricFld.fill(email);
+
+    await registrPage.annualEnergGasFld.fill(numbersVallues);
     //Drop down
     await registrPage.insertGasDropD.click(); 
     await registrPage.bioDisel.click(); // paramiter of dropdown
 
     await registrPage.averegeOutpTonnageFld.fill('23');
+    await page.pause();
     await registrPage.renewableEnergyFld.fill('23');
+    await page.getByRole('button', { name: 'Finish' }).click();
+
+    expect (await registrPage.UserOreadyExistErrorAllert).toBeDefined();
+    expect (await registrPage.userOlreadyExsErr).toBeDefined();
   });
