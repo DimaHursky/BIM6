40cf59fd277cec11e209b5aeda1271b5e5023039
DimaHursky <77124180+DimaHursky@users.noreply.github.com>
2023-12-06 23:54:59 +0200

    add the sammeries

---
+++ tests/materials.spec.js
@@ -0,0 +1,22 @@
+//Create valid material with filled all fields
+//Create material only with required fields
+//Create material when all fields are empty
+//Create matirial and Choose type of filling A level: A1
+//Create matirial and Choose type of filling A level: A2
+//Create matirial and Choose type of filling A level: A3
+//Create material and add hte PDF file
+//Fill all firlds and cansel creating the material on "CAnsel" btn
+//Fill all firlds and cansel creating the material on "X" button
+//Cansel creating the material with empty fields "CAnsel" btn
+//Cansel creating the material with empty fields "X" button
+//Groul materil by "All" status
+//Groul materil by "Approwed" status
+//Groul materil by "Waiting" status
+//Groul materil by "Rejected" status
+//Open created matirial
+//Open created matirial and click "See all" button
+//Edit material and save changes
+//Edit material and cansel changes
+//add coment to the material
+//use search to find material by the fuul name
+//use search to find material by the first part of the name
